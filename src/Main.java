23d62606e81ad0654f148cb75bd3a54fb32b799d
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-05-20 13:56:24 +0300

    init commit1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        Pattern pattern = Pattern.compile("((25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+        Pattern pattern = Pattern.compile("(((\\d|\\d\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3})(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)");
         Matcher matcher = pattern.matcher(input);
-        while(matcher.find())
+        if (matcher.find()){
             System.out.println(matcher.group());
+        }else {
+            System.out.println("Ошибка");
+        }
     }
 }
